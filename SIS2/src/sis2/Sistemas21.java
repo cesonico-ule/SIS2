ad5eb3a2c4da3f80ec96e6f58b4832583db4de75
Yago <Yago@Yago-Pc>
2021-03-18 20:46:42 +0100

    Fin dia J

--- SIS2/src/sis2/Sistemas21.java
+++ SIS2/src/sis2/Sistemas21.java
@@ -6,8 +6,10 @@
 package sis2;
 
 import Modelo.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -17,11 +19,11 @@ import org.hibernate.Transaction;
  *
  * @author Santiago Ares Alfayate
  */
-public class Sistemas21 {
+public class Sistemas2 {
 
     public static void main(String[] args) {
         //consultaDNI(peticionDatos());
-        consultaDNI("09741138");
+        consultaDNI("09741138V");
         
     }
     
@@ -34,14 +36,19 @@ public class Sistemas21 {
         return sc.nextLine();
     }
     
+    public static void actualizaNombreExcepto(Empresas empresaExcepcion){
+        System.out.println(empresaExcepcion.g)
+    }
     public static void consultaDNI (String dni){
         SessionFactory sf = null;
         Session sesion = null;
         Transaction tr = null;
-            
+        List<Integer> empresasNO = new ArrayList<Integer>();
+
         try{
             sf = HibernateUtil.getSessionFactory();
             sesion = sf.openSession();
+
             
             String consultaHQL = "FROM Trabajadorbbdd t WHERE t.nifnie = :param1"; //t es alias obligatorio
             
@@ -54,15 +61,31 @@ public class Sistemas21 {
             }
             else{
                 for(Trabajadorbbdd tbd: listaResultado){
-                    System.out.println("Nombre:\t" + tbd.getNombre());
-                    System.out.println("NIF:\t" + tbd.getNifnie());
-                    System.out.println("**************************");
+                    System.out.println("Nombre:\t\t" + tbd.getNombre());
+                    System.out.println("Apellidos:\t"+ tbd.getApellido1()+"\t"+tbd.getApellido2());
+                    System.out.println("NIF:\t\t" + tbd.getNifnie());
+                    //System.out.println("Fecha incorporacion:\t"+ tbd.getFechaAlta());
+                    System.out.println("Categoria:\t"+ tbd.getCategorias().getNombreCategoria());                    
+                    System.out.println("Empresa:\t"+ tbd.getEmpresas().getNombre()); 
+                    Set<Nomina> listaNominas =  tbd.getNominas();
+                    for(Nomina a: listaNominas ){
+                        System.out.println("Año:\t"+ a.getAnio()+"\tMes:\t"+a.getMes());
+                        System.out.printf("Bruto:\t"+a.getBrutoNomina()+" €\n");
+                    }
+                    System.out.println("*******************************");
+                    
+                    
+                    //Tarea 2
+                    empresasNO.add(tbd.getEmpresas().getIdEmpresa());
+                    
                 }
             }
+                        
             HibernateUtil.shutdown();
+            //Tarea 2
+            actualizaNombreExcepto(empresasNO);
         }
         catch(Exception e){
-            System.err.println("No está el horno para bollos" + e.getMessage());
+            System.err.println("No está el horno para bollos " + e.getMessage());
         }
     }
-}
